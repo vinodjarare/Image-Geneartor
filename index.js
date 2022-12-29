3b09df939fb3978ee38071dc7b1ea41fd30da264
Vinod Jarare <vsjarare@gmail.com>
2022-12-29 17:32:29 +0530

    added frontend

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(index.html);
+});
 
 app.use("/api", require("./routes/openaiRoutes"));
 
